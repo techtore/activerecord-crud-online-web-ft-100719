0361d1b4317bb9f623344518b5f7f91ac3f1b43e
Victoria Hale <vic.hale74@gmail.com>
2019-11-11 01:38:19 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,7 +30,7 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
     # release_date == 1990
   
   Movie.create do |m|
-    m.title = 
+    m.title = args[]
     m.release_date =  
   end
 end
